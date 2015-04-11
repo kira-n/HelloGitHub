9d9cfb5cad410edc2682a6110b9313b6bebb149c
Shion Nagano <sean634@icloud.com>
2015-04-11 16:04:29 +0900

    初めてのCommit&Push

--- calc/ViewController.h
+++ calc/ViewController.h
@@ -8,7 +8,34 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    
+    int number;
+    int number2;
+    int number3;
+    IBOutlet UILabel *label;
+    IBOutlet UILabel *label2;
+    IBOutlet UILabel *label3;
+    int ope;
+
+}
+-(IBAction)bt1;
+-(IBAction)bt2;
+-(IBAction)bt3;
+-(IBAction)bt4;
+-(IBAction)bt5;
+-(IBAction)bt6;
+-(IBAction)bt7;
+-(IBAction)bt8;
+-(IBAction)bt9;
+-(IBAction)bt0;
+-(IBAction)plus;
+-(IBAction)minus;
+-(IBAction)kakeru;
+-(IBAction)waru;
+-(IBAction)equal;
+-(IBAction)clear;
+-(IBAction)zeikomi;
 
 
 @end
--- calc/ViewController.m
+++ calc/ViewController.m
@@ -14,6 +14,157 @@
 
 @implementation ViewController
 
+
+-(IBAction)bt1{
+    if(ope==0){
+        number=number*10+1;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+1;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+
+-(IBAction)bt2{
+    if(ope==0){
+        number=number*10+2;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+2;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+-(IBAction)bt3{
+    if(ope==0){
+        number=number*10+3;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+3;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+
+-(IBAction)bt4{
+    if(ope==0){
+        number=number*10+4;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+4;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+-(IBAction)bt5{
+    if(ope==0){
+        number=number*10+5;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+5;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+-(IBAction)bt6{
+    if(ope==0){
+        number=number*10+6;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+6;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+-(IBAction)bt7{
+    if(ope==0){
+        number=number*10+7;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+7;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+
+-(IBAction)bt8{
+    if(ope==0){
+        number=number*10+8;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+8;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+
+-(IBAction)bt9{
+    if(ope==0){
+        number=number*10+9;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+9;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+
+-(IBAction)bt0{
+    if(ope==0){
+        number=number*10+0;
+        label.text=[NSString stringWithFormat:@"%d",number];
+    }
+    else{
+        number2=number2*10+0;
+        label.text=[NSString stringWithFormat:@"%d",number2];
+    }
+}
+-(IBAction)plus{
+    ope=1;
+}
+-(IBAction)minus{
+    ope=2;
+}
+-(IBAction)kakeru{
+    ope=3;
+}
+-(IBAction)waru{
+    ope=4;
+}
+-(IBAction)zeikomi{
+    ope=5;
+}
+-(IBAction)equal{
+    if(ope==1){
+        number3=number+number2;
+    }
+    else if(ope==2){
+        number3=number-number2;
+    }
+    else if(ope==3){
+        number3=number*number2;
+    }
+    else if(ope==4){
+        number3=number/number2;
+    }
+    else if(ope==5){
+        number3=number*1.08;
+    }
+    label.text=[NSString stringWithFormat:@"%d",number3];
+}
+-(IBAction)clear{
+    number=0;
+    number2=0;
+    number3=0;
+    ope=0;
+    label.text=[NSString stringWithFormat:@"%d",number];
+    label2.text=[NSString stringWithFormat:@"%d",number2];
+    label3.text=[NSString stringWithFormat:@"%d",number3];
+    
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
